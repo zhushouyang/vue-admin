6f9a2975dee2c9e54f073a8d7a904a4ed8c95e3a
zhusy@asiainfo.com <zhusy@asiainfo.com>
2018-03-21 23:41:55 +0800

    for chenchen java learning

---
+++ src/main/java/com/chenchen/learn/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.chenchen.learn;
+
+/**
+ * @author zhushouyang
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
