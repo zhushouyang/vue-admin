be63145e4de097c51fc30d8c4953d80b37f52246
zhusy@asiainfo.com <zhusy@asiainfo.com>
2018-04-29 13:18:59 +0800

    update 项目结构

--- src/main/java/com/zhushouyang/service/impl/LoginServiceImpl.java
+++ src/main/java/com/zhushouyang/service/impl/LoginServiceImpl.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.zhushouyang.dao.UserDao;
-import com.zhushouyang.mapper.UserMapper;
 import com.zhushouyang.model.User;
 import com.zhushouyang.service.LoginService;
 
